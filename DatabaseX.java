90e7777695d6a28f70494d847cada172f5b81842
Albert Corominas <albertcorominasmariscot@gmail.com>
2023-06-02 10:40:11 +0200

    Changes to DatabaseX structure and comments

--- DatabaseX.java
+++ DatabaseX.java
@@ -8,234 +8,307 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class DatabaseX {
+
+    private Printer p;
+
+    private Vector<Table> tables;
+
+    private Options o;
+
+    private Scanner sc;
+
+    private static DatabaseX db = new DatabaseX();
+
+    /**
+     @brief Main function of the program
+     @param args        Arguments of the program
+     @return Returns nothing.
+     */
     public static void main(String[] args){
 
-        Printer p = new Printer();
-        Vector<Table> tables = new Vector<>();
-        Options o = new Options(p);
-        SyntaxChecker s = new SyntaxChecker(p);
+        db.p = new Printer();
+        db.tables = new Vector<>();
+        db.o = new Options(db.p);
+        db.sc = new Scanner(System.in);
 
         //loadDummyData(tables);
-        loadOptions(o);
-        loadTablesFromFiles(tables);
-        Scanner sc = new Scanner(System.in);
-        printWelcome(p);
-        printMainMenu(p);
-        String option = askForString();
+        loadOptions();
+        loadTablesFromFiles();
+        printWelcome();
+        printMainMenu();
+        int option = askForInt(true,true,8,0);
         while(option!=0){
             switch(option) {
                 case 1:
-                    CreateTable(p,tables);
+                    CreateTable();
                     break;
                 case 2:
-                    SelectTable(p,tables);
+                    SelectTable();
                     break;
                 case 3:
-                    RemoveTable(p,tables);
+                    RemoveTable();
                     break;
                 case 4:
-                    CreateRegister(p,tables);
+                    CreateRegister();
                     break;
                 case 5:
-                    RemoveRegister(p,tables);
+                    RemoveRegister();
                     break;
                 case 6:
-                    RollbackLast(p,tables,o);
+                    RollbackLast();
                     break;
                 case 7:
-                    OptionsMenu(p,sc,o);
+                    OptionsMenu();
                     break;
                 case 8:
-                    StatsMenu(p,tables);
+                    StatsMenu();
                     break;
                 case 9:
-                    StatsTables(p,tables);
+                    StatsTables();
                     break;
             }
-            option = askForInt(p,sc,true,true,8,0);
+            option = askForInt(true,true,8,0);
         }
-        saveTablesOnFiles(tables);
-        p.println("END OF PROGRAM");
-        p.println("Made by Albert Corominas Mariscot");
+        saveTablesOnFiles();
+        db.p.println("END OF PROGRAM");
+        db.p.println("Made by Albert Corominas Mariscot");
     }
 
     /**
-     @brief Empty constructor for the options of the program
-     @param p           Printer that will be used to print info about the options
-     @return Returns Options Object.
+     @brief Creates a new Table
+     @return Returns nothing.
      */
-    public static void CreateTable(Printer p, Vector<Table> tables){
+    public static void CreateTable(){
 
     }
 
-    public static void SelectTable(Printer p, Vector<Table> tables){
+    /**
+     @brief Select Table
+     @return Returns nothing.
+     */
+    public static void SelectTable(){
 
     }
 
-    public static void RemoveTable(Printer p, Vector<Table> tables){
+    /**
+     @brief Remove a Table
+     @return Returns nothing.
+     */
+    public static void RemoveTable(){
 
     }
 
-    public static void CreateRegister(Printer p, Vector<Table> tables){
+    public static void CreateRegister(){
 
     }
 
-    public static void RemoveRegister(Printer p, Vector<Table> tables){
+
+    public static void RemoveRegister(){
 
     }
 
-    public static void RollbackLast(Printer p, Vector<Table> tables, Options o){
-        if(o.getRollbackFunction()) {
+    /**
+     @brief Rollbacks the last action
+     @return Returns nothing.
+     */
+    public static void RollbackLast(){
+        if(db.o.getRollbackFunction()) {
 
         } else {
-            p.println("This option is currently disabled");
+            db.p.println("This option is currently disabled");
         }
     }
 
-    public static void StatsMenu(Printer p, Vector<Table> tables){
+    public static void StatsMenu(){
 
     }
 
-    public static void StatsTables(Printer p, Vector<Table> tables){
+    public static void StatsTables(){
 
     }
 
-    public static void printWelcome(Printer p){
-        p.println("Welcome user");
+    /**
+     @brief Prints the welcome message
+     @return Returns nothing.
+     */
+    public static void printWelcome(){
+        db.p.println("Welcome user");
     }
 
-    public static void printMainMenu(Printer p){
-        p.println("{ 0 }: Exit Application");
-        p.println("{ 1 }: Create Table");
-        p.println("{ 2 }: Select Table");
-        p.println("{ 3 }: Remove Table");
-        p.println("{ 4 }: Create Register");
-        p.println("{ 5 }: Remove Register");
-        p.println("{ 6 }: Rollback Last Action");
-        p.println("{ 7 }: Options Menu");
-        p.println("{ 8 }: Stats Menu");
-        p.println("{ 9 }: Briefing of Tables");
+    /**
+     @brief Prints the main menu options
+     @return Returns nothing.
+     */
+    public static void printMainMenu(){
+        db.p.println("{ 0 }: Exit Application");
+        db.p.println("{ 1 }: Create Table");
+        db.p.println("{ 2 }: Select Table");
+        db.p.println("{ 3 }: Remove Table");
+        db.p.println("{ 4 }: Create Register");
+        db.p.println("{ 5 }: Remove Register");
+        db.p.println("{ 6 }: Rollback Last Action");
+        db.p.println("{ 7 }: Options Menu");
+        db.p.println("{ 8 }: Stats Menu");
+        db.p.println("{ 9 }: Briefing of Tables");
     }
 
-    public static int askForInt(Printer p, Scanner sc, boolean max, boolean min, int valMax, int valMin){
+    /**
+     @brief Asks for a Integer
+     @param max         Boolean that indicates if the Integer has a maximum value or not
+     @param min         Boolean that indicates if the Integer has a minimum value or not
+     @param valMax      Maximum value of the Integer
+     @param valMin      Minimum value of the Integer
+     @return Returns the Integer asked.
+     */
+    public static int askForInt(boolean max, boolean min, int valMax, int valMin){
         int aux;
         if(max && min){
-            p.println("Enter a Integer number (between "+valMin+" and "+valMax+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Integer number (between "+valMin+" and "+valMax+"):");
+            aux = db.sc.nextInt();
             while(aux<valMin || aux>valMax){
-                p.println("Enter a Integer number (between "+valMin+" and "+valMax+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Integer number (between "+valMin+" and "+valMax+"):");
+                aux = db.sc.nextInt();
             }
         } else if(max){
-            p.println("Enter a Integer number (maximum "+valMax+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Integer number (maximum "+valMax+"):");
+            aux = db.sc.nextInt();
             while(aux>valMax){
-                p.println("Enter a Integer number (maximum "+valMax+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Integer number (maximum "+valMax+"):");
+                aux = db.sc.nextInt();
             }
         } else {
-            p.println("Enter a Integer number (minimum "+valMin+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Integer number (minimum "+valMin+"):");
+            aux = db.sc.nextInt();
             while(aux<valMin){
-                p.println("Enter a Integer number (minimum "+valMin+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Integer number (minimum "+valMin+"):");
+                aux = db.sc.nextInt();
             }
         }
         return aux;
     }
 
-    public static double askForDouble(Printer p, Scanner sc, boolean max, boolean min, double valMax, double valMin){
+    /**
+     @brief Asks for a Double
+     @param max         Boolean that indicates if the Double has a maximum value or not
+     @param min         Boolean that indicates if the Double has a minimum value or not
+     @param valMax      Maximum value of the Double
+     @param valMin      Minimum value of the Double
+     @return Returns the Double entered by the user.
+     */
+    public static double askForDouble(boolean max, boolean min, double valMax, double valMin){
         double aux;
         if(max && min){
-            p.println("Enter a Double number (between "+valMin+" and "+valMax+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Double number (between "+valMin+" and "+valMax+"):");
+            aux = db.sc.nextInt();
             while(aux<valMin || aux>valMax){
-                p.println("Enter a Double number (between "+valMin+" and "+valMax+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Double number (between "+valMin+" and "+valMax+"):");
+                aux = db.sc.nextInt();
             }
         } else if(max){
-            p.println("Enter a Double number (maximum "+valMax+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Double number (maximum "+valMax+"):");
+            aux = db.sc.nextInt();
             while(aux>valMax){
-                p.println("Enter a Double number (maximum "+valMax+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Double number (maximum "+valMax+"):");
+                aux = db.sc.nextInt();
             }
         } else {
-            p.println("Enter a Double number (minimum "+valMin+"):");
-            aux = sc.nextInt();
+            db.p.println("Enter a Double number (minimum "+valMin+"):");
+            aux = db.sc.nextInt();
             while(aux<valMin){
-                p.println("Enter a Double number (minimum "+valMin+"):");
-                aux = sc.nextInt();
+                db.p.println("Enter a Double number (minimum "+valMin+"):");
+                aux = db.sc.nextInt();
             }
         }
         return aux;
     }
 
-    public static String askForString(Printer p, Scanner sc, boolean limited, Vector<String> s){
+    /**
+     @brief Asks for a String
+     @param limited     Boolean that indicates if the String options are limited or not
+     @param s           Vector of Strings that contains the possible Strings to enter
+     @return Returns the String entered by the user.
+     */
+    public static String askForString(boolean limited, Vector<String> s){
         String aux = "";
         if(limited){
             while(!s.contains(aux)){
                 System.out.print("Enter a String number from the list (");
                 for(int i = 0; i<s.size(); i++){
-                    p.print(s.elementAt(i));
+                    db.p.print(s.elementAt(i));
                 }
-                p.println("):");
-                aux = sc.nextLine();
+                db.p.println("):");
+                aux = db.sc.nextLine();
             }
 
         } else {
-            p.println("Enter a String:");
-            aux = sc.nextLine();
+            db.p.println("Enter a String:");
+            aux = db.sc.nextLine();
         }
         return aux;
     }
 
-    public static void loadDummyData(Printer p, Vector<Table> tables){
-        p.println("Dummy data is being loaded on the system ...");
+    /**
+     @brief Loads the dummy data on the system
+     @return Returns nothing.
+     */
+    public static void loadDummyData(){
+        db.p.println("Dummy data is being loaded on the system ...");
 
 
-        p.println("Data load complete!");
+        db.p.println("Data load complete!");
     }
 
-    public static void saveTablesOnFiles(Vector<Table> tables){
+    /**
+     @brief Saves the database tables on files
+     @return Returns nothing.
+     */
+    public static void saveTablesOnFiles(){
 
     }
 
-    public static void loadTablesFromFiles(Vector<Table> tables){
+    /**
+     @brief Loads the database tables from files
+     @return Returns nothing.
+     */
+    public static void loadTablesFromFiles(){
 
     }
 
-    public static void loadOptions(Options o){
+    public static void loadOptions(){
 
     }
 
-    public static void OptionsMenu(Printer p, Scanner sc, Options o){
-        printOptionsMenu(p);
-        int option = askForInt(p,sc,true,true,3,0);
+    /**
+     @brief Prints the options menu and asks for an option
+     @return Returns nothing.
+     */
+    public static void OptionsMenu(){
+        printOptionsMenu();
+        int option = askForInt(true,true,3,0);
         int n;
         while(option!=0) {
             switch (option) {
                 case 1:
-                    o.displayOptions();
+                    db.o.displayOptions();
                     break;
                 case 2:
-                    n = askForInt(p,sc,true,true,o.getSize(),1);
-                    o.infoOption(n);
+                    n = askForInt(true,true,db.o.getSize(),1);
+                    db.o.infoOption(n);
                     break;
                 case 3:
-                    n = askForInt(p,sc,true,true,o.getSize(),1);
-                    o.setOption(n);
+                    n = askForInt(true,true,db.o.getSize(),1);
+                    db.o.setOption(n);
                     break;
             }
-            printOptionsMenu(p);
-            option = askForInt(p,sc,true,true,3,0);
+            printOptionsMenu();
+            option = askForInt(true,true,3,0);
         }
     }
 
-    public static void printOptionsMenu(Printer p) {
-        p.println("{ 0 }: Exit Menu");
-        p.println("{ 1 }: Display Number Options");
-        p.println("{ 2 }: Display Info Options");
-        p.println("{ 3 }: Modify Options");
+    public static void printOptionsMenu() {
+        db.p.println("{ 0 }: Exit Menu");
+        db.p.println("{ 1 }: Display Number Options");
+        db.p.println("{ 2 }: Display Info Options");
+        db.p.println("{ 3 }: Modify Options");
     }
 }
